3531439d7a136c4bcc4145fdd81748d3b27ff0b3
Matthew Beckerleg <ord.sc2@gmail.com>
2020-01-13 10:04:36 -0800

    webpack build corrections

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Box } from 'grommet';
+import { Map, TileLayer } from 'react-leaflet';
 
 const Map = (props) => {
 
 	return (
 		<Box>
-      
+      <Map>
+        <TileLayer />
+      </Map>
 		</Box>
 	)
 };
